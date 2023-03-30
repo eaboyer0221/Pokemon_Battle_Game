ee081d8f3af60e2d9dc6f7c487afe604c12b07f1
eaboyer0221 <eaboyer0221@gmail.com>
2023-03-30 16:47:39 -0600

    Added battle screen.

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import math
 import random
+from tkinter import messagebox
+
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+from PIL import Image, ImageTk
+import io
+import tkinter as tk
+
+cached_windows = []
+cached_player = None
 
 
-
-def pokedex(url = 'https://pokemondb.net/pokedex/all'):
+def pokedex(url='https://pokemondb.net/pokedex/all'):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     table = soup.find('table', {'id': 'pokedex'})
@@ -24,32 +31,55 @@ def pokedex(url = 'https://pokemondb.net/pokedex/all'):
     return pd.DataFrame(rows, columns=headers)
 
 
-
-#Define Pokemon Class that has three attributes:
-#1) name    2) hit power   3) life points
+# Define Pokemon Class that has three attributes:
+# 1) name    2) hit power   3) life points
 class Pokemon:
-    def __init__(self, chosen_pokemon_name):
-        self.name = chosen_pokemon_name
-        #info = pokedex.loc[pokedex['Name'] == chosen_pokemon_name]
-        info = pokedex[pokedex['Name'] == chosen_pokemon_name].iloc[0]
-        self.hit_power = int(info['Attack'])
-        self.life_points = int(info['HP'])
+    def __init__(self, pokemon_data):
+        if pokemon_data is None:
+            pokemon_data = pokedex.sample()
+
+        self.name = pokemon_data['Name'].values[0]
+        self.pokemon_type = pokemon_data['Type'].values[0]
+        self.life_points = pokemon_data['HP'].values[0]
+        self.hit_power = pokemon_data['Attack'].values[0]
+        self.defense = pokemon_data['Defense'].values[0]
+        self.image_url = pokemon_data['image_url'].values[0]
         self.is_legendary = False
 
-    #Create "say_hi" method to print pokemon's basic info (name, hit power, and life points)
+    # Create "say_hi" method to print pokemon's basic info (name, hit power, and life points)
     def say_hi(self):
-        print(f"{self.name} says hello! {self.name} has {self.hit_power} hit power and {self.life_points} life points.")
+        messagebox.showinfo("Battle", f"{self.name} says hello! {self.name} has {self.hit_power} hit power and {self.life_points} life points.")
 
     # "attack" method that reduces the enemy's life point by the attacking pokemon's hit power, and prints out the enemy's life point after attack.
     def attack(self, enemy):
         enemy.life_points = max(enemy.life_points - self.hit_power, 0)
-        print(f"{self.name} attacks!")
-        print(f"{enemy.name} loses life power!")
-        print(f"{enemy.name} life power is {enemy.life_points}!")
+        return enemy.life_points
 
 
-#2. Define a child class (such as ElectricPokemon or WaterPokemon )
-# that has a more powerful attack than usual attack (such as reduce the enemy's life points by double its hit power).
+def handle_attack_click(player_pokemon, enemy_pokemon):
+    enemy_life_points = player_pokemon.attack(enemy_pokemon)
+    if enemy_life_points <= 0:
+        print(f"{enemy_pokemon.name} fainted. {player_pokemon.name} won the battle!")
+    else:
+        enemy_pokemon.attack(player_pokemon)
+        if player_pokemon.life_points <= 0:
+            print(f"{player_pokemon.name} fainted. {enemy_pokemon.name} won the battle!")
+        # print(f"{self.name} attacks!")
+        # print(f"{enemy.name} loses life power!")
+        # print(f"{enemy.name} life power is {enemy.life_points}!")
+
+        # let the player's Pokemon attack the enemy Pokemon until one of them loses the game
+        while player_pokemon.life_points > 0 and enemy_pokemon.life_points > 0:
+            player_pokemon.attack(enemy_pokemon)
+            if enemy_pokemon.life_points <= 0:
+                print(f"{enemy_pokemon.name} fainted. {player_pokemon.name} won the battle!")
+                break
+            enemy_pokemon.attack(player_pokemon)
+            if player_pokemon.life_points <= 0:
+                print(f"{player_pokemon.name} fainted. {enemy_pokemon.name} won the battle!")
+            break
+
+
 class Legendary(Pokemon):
     def __init__(self, name: str, hit_power: int, life_points: int):
         super().__init__(name, hit_power, life_points)
@@ -58,19 +88,11 @@ class Legendary(Pokemon):
             self.is_legendary = False
         self.hit_power *= 1.3
 
+
 def make_suitable_enemy_stat(self, pokemon_stat, percent_range_factor):
     percent_stat_adjustment = (percent_range_factor * pokemon_stat)
     return random.randint(pokemon_stat - percent_stat_adjustment, pokemon_stat + percent_stat_adjustment)
 
-def create_enemy():
-    enemy_pokemon = Pokemon(pokedex.sample()['Name'].iloc[0])
-    print(f"The enemy is a {enemy_pokemon.name} with hit power {enemy_pokemon.hit_power} and life points {enemy_pokemon.life_points}.")
-    return enemy_pokemon
-
-# define a function to prompt the player to choose a Pokemon to use in the fight
-def choose_pokemon():
-    chosen_pokemon_name = input("What's your Pokemon's name? ")
-    return Pokemon(chosen_pokemon_name)
 
 pokedex = pokedex()
 filter = pokedex['Name'].str.contains(" ")
@@ -79,32 +101,39 @@ print(pokedex)
 
 legendary_pokemon = ["Articuno", "Moltres", "Mewtwo", "Zapdos", "Lugia", "Ho-oh"]
 
+
+def display_pokemon(player_type, player, root):
+    # Create a new window to display the Pokemon info
+    if player_type == "player":
+        info_window = tk.Frame(root)
+        info_window.grid(row=6, column=1)
+    elif player_type == "enemy":
+        info_window = tk.Frame(root)
+        info_window.grid(row=6, column=3)
+
+    # Add labels and values to display the Pokemon info
+    tk.Label(info_window, text="Name:").grid(row=0, column=0)
+    pokemon = player.pokemon
+    tk.Label(info_window, text=pokemon.name).grid(row=0, column=1)
+    tk.Label(info_window, text="Type:").grid(row=1, column=0)
+    tk.Label(info_window, text=pokemon.pokemon_type).grid(row=1, column=1)
+    tk.Label(info_window, text="HP:").grid(row=2, column=0)
+    tk.Label(info_window, text=pokemon.life_points).grid(row=2, column=1)
+    tk.Label(info_window, text="Attack:").grid(row=3, column=0)
+    tk.Label(info_window, text=pokemon.hit_power).grid(row=3, column=1)
+    tk.Label(info_window, text="Defense:").grid(row=4, column=0)
+    tk.Label(info_window, text=pokemon.defense).grid(row=4, column=1)
+    # Add an image of the Pokemon to the window
+    response = requests.get(pokemon.image_url)
+    img = Image.open(io.BytesIO(response.content))
+    img = img.resize((100, 100))
+    img = ImageTk.PhotoImage(img)
+    tk.Label(info_window, image=img).grid(row=0, column=2, rowspan=5, padx=10)
+    player.pokemon_img = img  # save the img object as an instance variable
+    cached_windows.append(info_window)
+    return info_window
+
+
 if __name__ == "__main__":
-
-
     # create the player's Pokemon
     print("Welcome to the Pokemon battle!")
-    print("Choose your Pokemon:")
-    player_pokemon = choose_pokemon()
-    enemy_pokemon = create_enemy()
-
-    # print out their basic info
-    player_pokemon.say_hi()
-    enemy_pokemon.say_hi()
-
-    # let the player's Pokemon attack the enemy Pokemon until one of them loses the game
-    while player_pokemon.life_points > 0 and enemy_pokemon.life_points > 0:
-        player_pokemon.attack(enemy_pokemon)
-        if enemy_pokemon.life_points <= 0:
-            print(f"{enemy_pokemon.name} fainted. {player_pokemon.name} won the battle!")
-            break
-        enemy_pokemon.attack(player_pokemon)
-        if player_pokemon.life_points <= 0:
-            print(f"{player_pokemon.name} fainted. {enemy_pokemon.name} won the battle!")
-            break
-
-
-
-
-
-
